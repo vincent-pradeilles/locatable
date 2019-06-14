48edf79cadb166965f2b7e356a196155cc03ea9e
Vincent PRADEILLES <>
2019-06-14 14:44:34 +0200

    Add unit tests

--- Locatable/LocatableTests/LocatableTests.swift
+++ Locatable/LocatableTests/LocatableTests.swift
@@ -9,26 +9,27 @@
 import XCTest
 @testable import Locatable
 
+fileprivate protocol Servicing {
+    func action()
+}
+
+fileprivate class Service: Servicing {
+    func action() {
+        print("I'm performing a service 😊")
+    }
+}
+
 class LocatableTests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        Locator.factories.removeAll()
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    func testServiceLocating() {
+        Locator.register(Servicing.self, { return Service() })
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let service = Locator.locate(Servicing.self)
+        
+        XCTAssert(service is Service)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
