f0f1a11a30aba1825fbe90d3d3a7ae7aafbdab07
Vladislav Prusakov <spectraldragonchannel@gmail.com>
2019-10-06 03:44:15 +0300

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
             dependencies: []),
         .testTarget(
             name: "LocatableTests",
-            dependencies: ["Locatable"]),
+            dependencies: ["Locatable"]
         )
     ]
 )
